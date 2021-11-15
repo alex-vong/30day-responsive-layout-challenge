0907a367e27155df7dc95391b67b6201e89599d3
Alex Vong <avong003@gmail.com>
2021-11-15 11:52:24 -0800

    created index page

--- index.php
+++ index.php
@@ -10,5 +10,31 @@
 </section>
 
 
+<section class='page-section day-2'>
+	<inner-column>
+	 	<?php include('modules/starbucks.php'); ?> 
+	</inner-column>
+</section> 
+
+<section class='page-section day-3 forefathers'>
+	<inner-column>
+	 	<?php include('modules/forefathers.php'); ?> 
+	</inner-column>
+</section>
+
+<section class='page-section day-4 slack'>
+	<inner-column>
+	 	<?php include('modules/slack.php'); ?> 
+	</inner-column>
+</section>
+
+<section class='page-section day-5 pxl'>
+	<inner-column>
+	 	<?php include('modules/pxl.php'); ?> 
+	</inner-column>
+</section>
+
+
+
 
 <?php include('footer.php'); ?>
