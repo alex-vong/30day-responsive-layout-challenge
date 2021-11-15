702b670a2791dc21a14e0c6d11308aad47b2295d
Alex Vong <avong003@gmail.com>
2021-11-15 11:52:09 -0800

    pxl module

---
+++ modules/pxl.php
@@ -0,0 +1,108 @@
+ <h2 class="section-heading calm-voice">Day 5</h2>
+
+ <section class="project-info">
+    <h3 class="calm-voice">Source:
+         <a href="https://www.pxlagency.com/pxl-products" target="_blank">PXL Angency/PXL Products</a>
+<!--     </h3>
+        <h3 class="calm-voice"> Read the
+         <a href="https://alexvong.substack.com/p/responsive-layout-challenge-day-1" target="_blank">Case Study</a>
+    </h3> -->
+ </section>
+
+
+<section class="container">
+	<section class="site-header">
+		 <h1 class="loud-voice"> PXL&nbsp;Products</h1>
+
+ 		<picture>
+			<img src="images/plane.svg" alt="">
+		</picture> 
+	</section>
+
+
+<section class="pxl-grid">
+	<section class="pxl-platform">
+		<picture>
+			<img src="images/pxl-platform2.svg" alt="">
+		</picture>
+
+		<section class="details">
+			<h2 class="attention-voice">PXL Platform</h2>
+			<p class='quiet-voice'>The PXL Platform is a robust website system featuring a fully-custom content management interface. It’s designed to be used as the foundation for any enterprise B2C or B2B website, and is the backbone of sites that have been visited over 1 billion times since 2015.</p>
+		</section>
+	</section>
+
+	<section class="pxl-platform mobile">
+		
+		<picture>
+			<img src="images/pxl-mobile-gif.gif" alt="">
+		</picture>
+
+		<section class="details">
+			<h2 class="attention-voice">U Review</h2>
+			<p class='quiet-voice'>Amplify social chatter with U Review, a social media tool for quickly responding to positive comments with personalized high-end motion graphics.</p>
+		</section>
+	</section>
+
+	<section class="pxl-platform">
+		
+		<picture>
+			<img src="images/pxl-platform.jpeg" alt="">
+		</picture>
+
+		<section class="details">
+			<h2 class="attention-voice">Bracket Contest Platform</h2>
+			<p class='quiet-voice'>Quickly launch a “March Madness” inspired digital campaign with our online bracket generator and moderation tools. Fans can set their brackets, select their winner, and share their pick with the world.</p>
+		</section>
+	</section>
+
+	<section class="pxl-platform">
+		
+		<picture>
+			<img src="images/selfie.jpeg" alt="">
+		</picture>
+
+		<section class="details">
+			<h2 class="attention-voice">Red Carpet Selfie Booth</h2>
+			<p class='quiet-voice'>It’s more than a camera on the red carpet - the Selfie Booth produces fun, expressive branded social content with on-voice creative that is guaranteed to be shared by&nbsp;fans.
+				<br>
+				<br>
+			The Selfie Booth can be customized for each event with unique assets and frames, outputting both static images and animated videos, which make no two events the&nbsp;same!</p>
+		</section>
+	</section>
+
+	<section class="pxl-platform">
+		
+		<picture>
+			<img src="images/meme.jpeg" alt="">
+		</picture>
+
+		<section class="details">
+			<h2 class="attention-voice">Meme Generator</h2>
+			<p class='quiet-voice'>Make some memes! PXL’s Meme Generator web application gets fans sharing your content with a personal twist. Outputting either still or video content, fans can share their asset from the Meme Generator direct to Facebook and Twitter, or download with&nbsp;1-click.
+				<br>
+				<br>
+			The app can be configured to place user photos underneath themed content as well as add digital stickers and text on top.
+		</p>
+		</section>
+	</section>
+	
+	<section class="pxl-platform quiz-game">
+		
+		<picture>
+			<img src="images/pxl-quiz-game.png" alt="">
+		</picture>
+
+		<section class="details">
+			<h2 class="attention-voice">Quiz Game</h2>
+			<p class='quiet-voice'>Challenge fans with trivia or create a personalized experience with self-reflection questions using PXL’s multiple choice quiz game. The game can be played on Facebook or in the browser, ending with a sharable score asset to encourage word of mouth marketing.</p>
+		</section>
+	</section>
+</section>
+	
+
+
+
+
+
+</section>
