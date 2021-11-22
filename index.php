5aa307e0fa55d4a1501209adcd4379892aaba16a
Alex Vong <avong003@gmail.com>
2021-11-21 21:57:17 -0800

    reformatted spacing index, removed article data and exclude ds store

--- index.php
+++ index.php
@@ -2,39 +2,43 @@
 
 <?php include('header.php'); ?>
 
-
-<section class='page-section day-1'>
-	<inner-column>
-	 	<?php include('modules/day-1.php'); ?> 
-	</inner-column>
-</section>
-
-
-<section class='page-section day-2'>
-	<inner-column>
-	 	<?php include('modules/starbucks.php'); ?> 
-	</inner-column>
-</section> 
-
-<section class='page-section day-3 forefathers'>
-	<inner-column>
-	 	<?php include('modules/forefathers.php'); ?> 
-	</inner-column>
-</section>
-
-<section class='page-section day-4 slack'>
-	<inner-column>
-	 	<?php include('modules/slack.php'); ?> 
-	</inner-column>
-</section>
-
-<section class='page-section day-5 pxl'>
-	<inner-column>
-	 	<?php include('modules/pxl.php'); ?> 
-	</inner-column>
-</section>
+	<section class='page-section day-1'>
+		<inner-column>
+			<?php include('modules/day-1.php'); ?> 
+		</inner-column>
+	</section>
 
 
+	<section class='page-section starbucks'>
+		<inner-column>
+			<?php include('modules/starbucks.php'); ?> 
+		</inner-column>
+	</section> 
+
+	<section class='page-section day-3 forefathers'>
+		<inner-column>
+			<?php include('modules/forefathers.php'); ?> 
+		</inner-column>
+	</section>
+
+	<section class='page-section day-4 slack'>
+		<inner-column>
+			<?php include('modules/slack.php'); ?> 
+		</inner-column>
+	</section>
+
+	<section class='page-section day-5 pxl'>
+		<inner-column>
+			<?php include('modules/pxl.php'); ?> 
+		</inner-column>
+	</section> 
 
 
-<?php include('footer.php'); ?>
+	<section class='page-section day-6 upgrade'>
+		<inner-column>
+			<?php include('modules/upgrade.php'); ?> 
+		</inner-column>
+	</section>
+
+
+
